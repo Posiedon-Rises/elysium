8a7a1eef722e39bd4b8416b1e80a7358f1967c99
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-07-16 10:24:25 -0500

    Expand nvf config

--- modules/home/shells/programs/nvim/default.nix
+++ modules/home/shells/programs/nvim/default.nix
@@ -1,6 +1,18 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  devCfg = config.elysium.development;
+  langCfg = devCfg.languages;
+in
+{
+  # Enable fzf
+  elysium.shells.programs.fzf.enable = lib.mkDefault true;
+
   programs.neovim = {
     enable = true;
   };
@@ -9,6 +21,8 @@
     enable = true;
 
     settings.vim = {
+
+      # Visuals
       theme = {
         enable = true;
         name = "tokyonight";
@@ -18,6 +32,60 @@
       statusline.lualine = {
         enable = true;
         theme = "auto";
+        componentSeparator = {
+          left = "";
+          right = "";
+        };
+      };
+
+      # Programming
+
+      autocomplete.blink-cmp.enable = true;
+
+      lsp = {
+        enable = true;
+        formatOnSave = true;
+        inlayHints.enable = true;
+      };
+
+      languages = {
+        nix = lib.mkIf langCfg.nix.enable {
+          enable = true;
+          extraDiagnostics.enable = true;
+          extraDiagnostics.types = [
+            "deadnix"
+            "statix"
+          ];
+
+          lsp = {
+            server = "nixd";
+
+            options = {
+              formatting.command = "nix fmt";
+
+              nixos.expr = "(builtins.getFlake (\"git+file://\" + lib.elysium.relativeToRoot ./.)).nixosConfigurations.Hydra.options";
+              home-manager.expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.Hydra.options.home-manager.users.type.getSubOptions []";
+            };
+          };
+        };
+      };
+
+      # Other
+
+      telescope = {
+        enable = true;
+
+        extensions = [
+          {
+            name = "fzf";
+            packages = [ pkgs.vimPlugins.telescope-fzf-native-nvim ];
+            setup = {
+              fzf = {
+                fuzzy = true;
+              };
+            };
+          }
+        ];
       };
     };
   };
