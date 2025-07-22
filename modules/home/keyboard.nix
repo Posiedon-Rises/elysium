27740b9c31e6926a6ca8e7995ca0da8e5695a222
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-07-22 15:41:01 -0500

    Fix hyprland keyboard options

--- modules/home/keyboard.nix
+++ modules/home/keyboard.nix
@@ -13,5 +13,5 @@
       lib.optional config.elysium.shells.programs.neovim.enable "caps:swapescape";
   };
 
-  wayland.windowManager.hyprland.settings.input.kb_options = lib.mkDefault config.home.keyboard.options;
+  wayland.windowManager.hyprland.settings.input.kb_options = lib.mkDefault <| lib.concatStringsSep "," <| config.home.keyboard.options;
 }
