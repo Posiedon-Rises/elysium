cc824f5d73cd5192686dd623ddf2f2c2c5dc2311
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-07-22 15:42:44 -0500

    Work on oh-my-posh

--- modules/home/shells/programs/oh-my-posh/default.nix
+++ modules/home/shells/programs/oh-my-posh/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, vauxhall, ... }:
+{ config, lib, ... }:
 
 let 
   cfg' = config.elysium.shells.programs;
@@ -16,14 +16,65 @@ in {
 
       blocks = [
         {
+          newline = true;
           type = "prompt";
           alignment = "left";
-          leading_diamond = "";
-          trailing_diamond = "";
           segments = [
             {
-              type = "os";
-              style = "powerline";
+              type = "session";
+              style = "accordion";
+              leading_powerline_symbol = "";
+              foreground = "black";
+              background = "lightGreen";
+              template = " {{ if .SSHSession }} {{ end }}{{ .HostName }} ";
+            }
+            {
+              type = "session";
+              style = "accordion";
+              powerline_symbol = "";
+              foreground = "black";
+              background = "cyan";
+              template = " {{ if .Root }}󱐋{{ end }}{{ .UserName }} ";
+            }
+            {
+              type = "path";
+              style = "accordion";
+              powerline_symbol = "";
+              foreground = "black";
+              background = "lightBlue";
+              properties.style = "agnoster_short";
+            }
+            {
+              type = "git";
+              style = "accordion";
+              powerline_symbol = "";
+              foreground = "black";
+              background = "magenta";
+              template = " {{ .HEAD }}{{ if or (.Working.Changed) (.Staging.Changed) }}*{{ end }} {{ if gt .Behind 0 }}⇣{{ end }}{{ if gt .Ahead 0 }}⇡{{ end }} ";
+              properties = {
+                branch_icon = " ";
+                commit_icon = "@";
+                fetch_status = true;
+              };
+            }
+          ];
+        }
+
+        {
+          newline = true;
+          type = "prompt";
+          align = "left";
+          segments = [
+            {
+              type = "text";
+              style = "plain";
+
+              foreground_templates = [
+                "{{ if gt .Code 0 }}red{{ end }}"
+                "{{ if eq .Code 0 }}lightCyan{{ end }}"
+              ];
+              background = "transparent";
+              template = "";
             }
           ];
         }
